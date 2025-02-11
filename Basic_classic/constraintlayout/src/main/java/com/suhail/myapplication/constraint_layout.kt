4d84cf8efe727a86fd4bbab2a6a4979036477bd7
suhail-ak <suhailakramy12@gmail.com>
2025-02-11 20:32:17 +0100

    added image

--- Basic_classic/constraintlayout/src/main/java/com/suhail/myapplication/constraint_layout.kt
+++ Basic_classic/constraintlayout/src/main/java/com/suhail/myapplication/constraint_layout.kt
@@ -20,7 +20,7 @@ class constraint_layout : AppCompatActivity() {
         enableEdgeToEdge()
         // inflating by Binding
         binding = ActivityConstraintLayoutBinding.inflate(layoutInflater)
-          setContentView(binding.root)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
