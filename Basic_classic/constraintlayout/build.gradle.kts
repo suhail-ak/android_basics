ce44b37547e5480f8f3626db0bf38c68887be1fc
suhail-ak <suhailakramy12@gmail.com>
2025-02-11 17:58:37 +0100

    constraint trial

--- Basic_classic/constraintlayout/build.gradle.kts
+++ Basic_classic/constraintlayout/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.suhail.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.suhail.myapplication"
